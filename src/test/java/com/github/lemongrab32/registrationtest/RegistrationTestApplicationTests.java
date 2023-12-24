8a04d94e5cec1316ed936e156f4046935d16aa6a
IO3683 <nu-dopustim@mail.ru>
2023-12-24 15:36:43 +0600

    RT-0: Spring Boot project created

---
+++ src/test/java/com/github/lemongrab32/registrationtest/RegistrationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lemongrab32.registrationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
